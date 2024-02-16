a57d0b7deab8b09eff1203302ee3e6120f48432c
Paul Kum <kbpaul2016@gmail.com>
2024-02-17 01:57:57 +0400

    Complete the palindromes exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const string = str.replaceAll(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverseString = [...string].reverse().join('');
+    return string === reverseString;
 };
 
 // Do not edit below this line
