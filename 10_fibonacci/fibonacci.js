490b8b467ccfff7ce1c49c3d8cb5a382f9324685
Paul Kum <kbpaul2016@gmail.com>
2024-02-18 00:41:09 +0400

    Completed the Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS";
+    }
+    // return 1 if number is 0 or 1
+    if (number == 0 ){
+        return 0;
+    }
 
+    if (number == 1 ){
+        return 1;
+    }
+
+    let n1 = 1;
+    let n2 = 1;
+    let arr = [n1, n2];
+    
+    while(arr.length < number){
+        const temp = n1 + n2;
+        arr.push(temp);
+        n1 = n2;
+        n2 = temp;
+    }
+
+    return arr.pop();
 };
 
 // Do not edit below this line
