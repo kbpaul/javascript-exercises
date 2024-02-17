2e43deaec75594ea940c7ee7125ef35ddf3bd078
Paul Kum <kbpaul2016@gmail.com>
2024-02-18 01:28:41 +0400

    Compmleted the rest of the Javascript exercises
    The last exercises was a little challenging. However, I came to realize that the reduce method was the easiest way forward.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+    const findTheOldest = function(people) {
+        const oldestPerson = people.reduce((oldest, person) => {
+            const currentAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+            const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+            return currentAge > oldestAge ? person : oldest;
+        });
+    
+        return oldestPerson;
+    };      
 
 // Do not edit below this line
 module.exports = findTheOldest;
